fa375e89e7b2555c4461c3d3a59988a2654ce217
TegaBC <jjbencarew10@gmail.com>
2022-10-16 13:00:43 +0100

    added front end validation

---
+++ script.js
@@ -0,0 +1,22 @@
+const inputs = document.querySelectorAll("input");
+const formSubmit = document.querySelector("#submit")
+const passwordField = document.querySelector("#password")
+const passwordConfirmField = document.querySelector("#password-confirm")
+
+inputs.forEach((item, index) =>{
+    item.addEventListener("focus", () => {
+        item.classList.add("live-validation")
+    })
+})
+
+formSubmit
+
+function checkPasswordMatch(){
+    console.log("Running validation")
+    if (passwordField.value == passwordConfirmField.value){
+        return true
+    }
+   // passwordConfirmField.setCustomValidity("Passwords do not match")
+    return false
+}
+
